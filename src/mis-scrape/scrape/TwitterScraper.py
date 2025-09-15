21d0cb6dbce1c31ddf58c12e1aa6f165c5507b85
Aditya Shrivastava <adityashrivastava2799@gmail.com>
2025-09-15 07:08:32 +0000

    Revert "chore: publish remaining edits (tutorial.ipynb, TwitterScraper, recreate/)"
    This reverts commit ae086a2263e3f21e2b6356983b1be5f7189df7af.

--- src/mis-scrape/scrape/TwitterScraper.py
+++ src/mis-scrape/scrape/TwitterScraper.py
@@ -366,7 +366,6 @@ class TwitterScraper:
         import tweepy  # type: ignore
 
         token = os.getenv("X_BEARER")
-        print(token)
         if not token:
             raise RuntimeError("Set X_BEARER to your app-only bearer token")
 
